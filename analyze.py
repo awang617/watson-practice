947643013e1ff4bc969a3cd9556a67b74020f4a0
awang617 <wanga18@mail.wlu.edu>
2019-06-25 16:41:43 -0400

    changing around credentials

--- analyze.py
+++ analyze.py
@@ -1,2 +1,16 @@
 # api_key = "Slv2arJiA6DWHRfUHXyWa1Gi4kJsE0FUuaSf2lFnkLCb"
 # url = "https://gateway.watsonplatform.net/natural-language-understanding/api"
+
+import json
+from watson_developer_cloud import NaturalLanguageUnderstandingV1
+from watson_developer_cloud.natural_language_understanding_v1 import Features, EmotionOptions
+
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+    version='2018-11-16'
+)
+
+response = natural_language_understanding.analyze(
+    html="<html><head><title>Fruits</title></head><body><h1>Apples and Oranges</h1><p>I love apples! I don't like oranges.</p></body></html>",
+    features=Features(emotion=EmotionOptions(targets=['apples','oranges']))).get_result()
+
+print(json.dumps(response, indent=2))
